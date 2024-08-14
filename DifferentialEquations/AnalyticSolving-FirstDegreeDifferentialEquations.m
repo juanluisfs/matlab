1cf555820b0918a0b43ac968e2bdd1a01ce1beb8
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-08-13 23:22:20 -0600

    Update AnalyticSolving-FirstDegreeDifferentialEquations.m

--- DifferentialEquations/AnalyticSolving-FirstDegreeDifferentialEquations.m
+++ DifferentialEquations/AnalyticSolving-FirstDegreeDifferentialEquations.m
@@ -1,3 +1,9 @@
 %% Analytic Solving Method for First Degree Differential Equations
 %% Método analítico para resolver EDO primer orden
 
+% Clean screen and variables
+% Limpiar pantalla y variables
+clc
+clear
+format shortG
+
