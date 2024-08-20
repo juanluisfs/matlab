0abf6d8b32d5552d5419790e3faa4ef82161e7a5
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-08-19 19:03:46 -0600

    Update 4thOrderRungeKutta.m

--- DifferentialEquations/4thOrderRungeKutta.m
+++ DifferentialEquations/4thOrderRungeKutta.m
@@ -1,29 +1,34 @@
 %% 4th order - Runge Kutta Method
 %% Método Runge - Kutta (4º Orden)
 
-%Limpiar pantalla y variables
+% Clean screen and variables
+% Limpiar pantalla y variables
 clc
 clear
 format shortG
 
-%Datos iniciales
+% Initial data
+% Datos iniciales
 f = @(x,y) 0.026*(1-y/12000)*y;
 x0 = 2000;
 y0 = 6079;
 xf = 2025;
 n = 2;
 
-%Inicia el método
+% Start method
+% Inicia el método
 h = (xf-x0)/n;
 vx(1) = x0;
 vy(1) = y0;
-    
-%Llenar el vector x
+
+% Fill the x vector
+% Llenar el vector x
 for i = 1:n
     vx(i+1) = vx(i) + h;
 end
 
-%Llenar el vector y
+% Fill the y vector
+% Llenar el vector y
 for i=1:n
     k1 = f(vx(i),vy(i));
     k2 = f(vx(i)+(h/2),vy(i)+((h/2)*k1));
@@ -32,15 +37,18 @@ for i=1:n
     vy(i+1) = vy(i) + (h/6)*(k1+(2*k2)+(2*k3)+k4);
 end
 
-%Solución Analítica
+% Analytic solution
+% Solución Analítica
 [valx,valy]=ode45(f,[vx],y0);
 
-%Impresión de resultados
+% Print results
+% Impresión de resultados
 disp('            x      y aprox       y real        Error  ')
 disp('------------------------------------------------------')
 disp([vx',vy',valy, abs((valy-vy')./valy)*100])
 
-%Graficar los puntos obtenid
+% Graph the obtained points
+% Graficar los puntos obtenidos
 clf
 hold on
 plot(vx, valy, 'LineWidth', 2, 'color', 'b',"Marker",'square')
