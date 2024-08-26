d57b0a1c4371ed591eb81b71949a5aa4332063d5
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-08-26 15:52:44 -0600

    Update and rename YDeltaConverter.m to StarToDeltaConverter.m

--- Y Delta Converter Calculator/StarToDeltaConverter.m
+++ Y Delta Converter Calculator/StarToDeltaConverter.m
@@ -3,25 +3,16 @@
 
 format shortEng
 
+% Capture Star (Y) Resistor Values
+% Capturar los valores de las resistencia Estrella (Y)
 r1 = 1300
 r2 = 2100
 r3 = 4780
 
 suma = (r1*r2)+(r2*r3)+(r3*r1);
+
+% Print Star (Y) Results
+% Imprimir resultados Estrella (Y)
 ra = suma/r2
 rb = suma/r3
 rc = suma/r1
-
-%% From Delta to Star (Y)
-%% De Delta a Estrella
-
-format shortEng
-
-ra = 33
-rb = 20
-rc = 21
-
-suma = ra+rb+rc;
-r1 = (ra*rb)/suma
-r2 = (rb*rc)/suma
-r3 = (rc*ra)/suma
