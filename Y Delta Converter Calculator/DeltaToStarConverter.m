7d95aac5db344e2f5730c7bb2622d26ee3c50622
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-08-26 15:53:08 -0600

    Create DeltaToStarConverter.m

---
+++ Y Delta Converter Calculator/DeltaToStarConverter.m
@@ -0,0 +1,17 @@
+%% From Delta to Star (Y)
+%% De Delta a Estrella
+
+format shortEng
+
+% Capture Delta Resistor Values
+% Capturar los valores de las resistencias Delta
+ra = 33
+rb = 20
+rc = 21
+
+% Print Delta Results
+% Imprimir resultados Delta
+suma = ra+rb+rc;
+r1 = (ra*rb)/suma
+r2 = (rb*rc)/suma
+r3 = (rc*ra)/suma
