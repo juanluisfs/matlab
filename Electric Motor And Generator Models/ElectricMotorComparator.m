cfc65ce88efc2400855bed6cb7fa469703796318
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-10-21 22:04:23 -0600

    Update ElectricMotorComparator.m

--- Electric Motor And Generator Models/ElectricMotorComparator.m
+++ Electric Motor And Generator Models/ElectricMotorComparator.m
@@ -4,7 +4,7 @@ clc
 clear
 
 % Ask user for the coil initial data
-%
+% Preguntar al usuario por los datos iniciales de la bobina
 prompt = {'Campo Magnético [T]','Área de la Espira [m2]','Número de Vueltas de la Bobina','Voltaje Aplicado'};
 digtitle = 'Introduce los Valores del Primer Motor';
 dims = [1 25];
